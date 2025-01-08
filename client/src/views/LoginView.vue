411ab55a60e3936791598ff4ee05bd768da1e660
clari <blue.atlantic09@gmail.com>
2025-01-08 20:25:35 +0800

    complete the logic in authenticating

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-    <h2 class="text-center mb-[10px]">Login Form</h2>
-    <form @submit.prevent="handleSubmit">
+    <h2 class="text-center">Login Form</h2>
+    <form @submit.prevent="handleSubmit" v-if="!isLoggedIn">
       <div>
-        <!-- <label for="username">Username:</label> -->
-        <input
-          type="text"
-          id="username"
-          v-model="username"
-          placeholder="Username"
-          required
-        />
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required />
       </div>
       <div>
-        <!-- <label for="password">Password:</label> -->
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          placeholder="Password"
-          required
-        />
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
       </div>
       <button type="submit">Login</button>
     </form>
+
+    <div v-if="isLoggedIn">
+      <h3>Welcome, {{ username }}!</h3>
+      <button v-if="isEditor" @click="addUser">Add User</button>
+      <button @click="logout">Logout</button>
+    </div>
+
     <div v-if="message">{{ message }}</div>
   </div>
 </template>
@@ -34,29 +29,52 @@ export default {
     return {
       username: "",
       password: "",
+      isLoggedIn: false,
+      isEditor: false,
       message: "",
     };
   },
+  created() {
+    const storedUser = localStorage.getItem("loggedInUser");
+    const storedRole = localStorage.getItem("userRole");
+    if (storedUser) {
+      this.username = storedUser;
+      this.isLoggedIn = true;
+      this.isEditor = storedRole === "editor";
+    }
+  },
   methods: {
     handleSubmit() {
-      // Simulated authentication logic
-      if (
-        this.username === "user_writer" &&
-        this.password === "writer_password"
-      ) {
-        this.message = `Welcome Writer ${this.username}!`;
-        // Add writer-specific logic here
+      if (this.username === "user_writer" && this.password === "writer12345") {
+        this.isLoggedIn = true;
+        this.isEditor = false;
+        localStorage.setItem("loggedInUser", this.username);
+        localStorage.setItem("userRole", "writer");
       } else if (
         this.username === "user_editor" &&
-        this.password === "editor_password"
+        this.password === "editor12345"
       ) {
-        this.message = `Welcome Editor ${this.username}!`;
-        // Add editor-specific logic here
+        this.isLoggedIn = true;
+        this.isEditor = true;
+        localStorage.setItem("loggedInUser", this.username);
+        localStorage.setItem("userRole", "editor");
       } else {
         this.message = "Invalid credentials. Please try again.";
       }
       console.log("Username:", this.username);
       console.log("Password:", this.password);
+    },
+    addUser() {
+      alert("User added successfully!");
+    },
+    logout() {
+      this.isLoggedIn = false;
+      this.isEditor = false;
+      this.username = "";
+      this.password = "";
+      localStorage.removeItem("loggedInUser");
+      localStorage.removeItem("userRole");
+      this.message = "You have logged out.";
     },
   },
 };
@@ -70,6 +88,9 @@ export default {
   border: 1px solid #ccc;
   border-radius: 5px;
 }
+h2 {
+  margin-bottom: 10px;
+}
 
 div {
   margin-bottom: 1em;
@@ -77,13 +98,14 @@ div {
 
 label {
   margin-bottom: 0.5em;
-  color: #333333;
+  color: #ccc;
   display: block;
 }
 
 input {
   border: 1px solid #cccccc;
   background: transparent;
+  border-radius: 5px;
   width: 100%;
 }
 
